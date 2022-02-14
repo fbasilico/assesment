11d3d53834aa6328d9ed33197323d86e95883dad
Franco Basilico <franbasilico@gmail.com>
2022-02-14 14:24:56 +0000

    assesment rename

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# T&t Assesment
+# T&t assessment
 
 ## Run app
 
--- apps/assessment/jest.config.js
+++ apps/assessment/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'assesment',
+  displayName: 'assessment',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -8,7 +8,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/apps/assesment',
+  coverageDirectory: '../../coverage/apps/assessment',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
